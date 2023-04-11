a9900c4e900cbc8162ceb3cf7d884e3b2c1c709d
Bartosz Janowski <detnotatka@gmail.com>
2023-04-11 22:16:08 +0200

    Initial commit

---
+++ pum6.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+import os
+import pandas as pd 
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import LabelEncoder
+
+frame = tk.Tk()
+frame.title("s23375's, very original and not taken from the tutorial, app for NN")
+frame.geometry('400x200')
+
+label = tk.Label(frame, text = "")
+# pack() - showing the label to the user
+label.pack()
+
+# provide a new function where user will provide the no of epochs
+def main():
+    # input from the user (only takes input from the first object without new line mark)
+    inp = int(numberofep.get(1.0,"end-1c"))
+    label.config(text = "Number of iterations provided: {inp}")
+    data = pd.read_csv("DSP_1.csv")
+
+    cols = ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+    data = data[cols].copy()
+
+    data["Age"].fillna((data["Age"].mean()), inplace=True)
+    data.dropna(subset=['Embarked'], inplace=True)
+
+    encoder = LabelEncoder()
+
+    data.loc[:,"Sex"] = encoder.fit_transform(data.loc[:,"Sex"])
+    data.loc[:,"Embarked"] = encoder.fit_transform(data.loc[:,"Embarked"])
+
+    y_train = data.iloc[0:600:,0]
+    y_test = data.iloc[600:700:,0]
+    X_train = data.iloc[0:600:,1:8]
+    X_test = data.iloc[600:700:,1:8]
+
+    model = Sequential()
+
+    if int(inp) > 300:
+        model.add(Dense(20, kernel_initializer = "uniform", activation="relu", input_dim = 7))
+        model.add(Dense(10, kernel_initializer = "uniform", activation="relu"))
+        model.add(Dense(8, kernel_initializer = "uniform", activation="relu"))
+        model.add(Dense(8, kernel_initializer = "uniform", activation="relu"))
+        model.add(Dense(6, kernel_initializer = "uniform", activation="relu"))
+        model.add(Dense(1, kernel_initializer = "uniform", activation="sigmoid"))
+        model.compile(optimizer = 'adam', loss = "binary_crossentropy", metrics = ['accuracy'])
+        model.fit(X_train, y_train, batch_size = 30, epochs = int(inp))
+        print(model.summary())
+    else:
+        model.add(Dense(10, kernel_initializer = "uniform", activation="relu", input_dim = 7))
+        model.add(Dense(7, kernel_initializer = "uniform", activation="relu"))
+        model.add(Dense(7, kernel_initializer = "uniform", activation="relu"))
+        # it has to be 1 cause we only want to provide one thing (if survived)
+        model.add(Dense(1, kernel_initializer = "uniform", activation="sigmoid"))
+        model.compile(optimizer = 'adam', loss = "binary_crossentropy", metrics = ['accuracy'])
+        model.fit(X_train, y_train, batch_size = 30, epochs = int(inp))
+        print(model.summary())
+    model.save('model')
+
+numberofep = tk.Text(
+    frame,
+    height = 5,
+    width = 20
+)
+numberofep.pack()
+
+newButton = tk.Button(frame, text = "Go!", command = main)
+newButton.pack()
+frame.mainloop()
